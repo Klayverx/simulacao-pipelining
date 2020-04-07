09d2428a63fad67d2c8f5ea2ec38f490a53cd027
Klayverx <klayverx@gmail.com>
2020-04-07 20:37:01 -0300

    Adicionando comentários e pequenas mudanças

--- src/simulacao/Simulacao_Thread.java
+++ src/simulacao/Simulacao_Thread.java
@@ -4,31 +4,33 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
- * @author Klayver Ximenes
+ *  Klayver Ximenes Carmo                    Mat.: 427651
+ *  Francisca Jannielly Garcia da Costa      Mat.: 427463
  */
 public class Simulacao_Thread {
     
-    static int quantidade = 5;
-    static int instrucao_atual = 1;
+    static int quantidade = 5;              //quantidade de etapas (Threads)
+    static int instrucao_atual = 1;         //instrução atual
     
     public static void main(String[] args) throws InterruptedException {
         
+        //condição para que a simulação funcione até a primeira instrução percorrer todas as etapas
         while(instrucao_atual <= quantidade){
             
+            //primeira Thread
             if(instrucao_atual >= 1){
-                
                 Thread s1 = new Thread(){
                     @Override
                     public void run() {
-                        tempo();
+                        tempo();        //simulação do tempo de processamento
                         System.out.println("Buscando Instrução I" +instrucao_atual);
                     }
                 };
-                s1.start();
-                s1.join();
+                s1.start();     //inicializando a Thread
+                s1.join();      //a instrução só irá para a outra etapa após terminar essa
             }
             
+            //segunda Thread
             if(instrucao_atual >= 2){
                 Thread s2 = new Thread(){
                     @Override
@@ -41,6 +43,7 @@ public class Simulacao_Thread {
                 s2.join();
             }
             
+            //terceira Thread
             if(instrucao_atual >= 3){
                 Thread s3 = new Thread(){
                     @Override
@@ -53,6 +56,7 @@ public class Simulacao_Thread {
                 s3.join();
             }
             
+            //quarta Thread
             if(instrucao_atual >= 4){
                 Thread s4 = new Thread(){
                     @Override
@@ -65,6 +69,7 @@ public class Simulacao_Thread {
                 s4.join();
             }
 
+            //quinta Thread
             if(instrucao_atual >= 5){
                 Thread s5 = new Thread(){
                     @Override
@@ -77,13 +82,14 @@ public class Simulacao_Thread {
                 s5.join();
             }
             
-            instrucao_atual +=1;
+            instrucao_atual ++;        //incremento no contador da posição da instrução
             
             tempo();
             System.out.println("\n-----------------------\n");
         }
     }
     
+    //função do tempo que foi dado na simulação do tempo de cada estágio
     public static void tempo() {
         try {
             Thread.sleep(1000);
